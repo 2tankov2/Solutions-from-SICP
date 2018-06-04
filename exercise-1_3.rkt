a8a57fe58405abb53cdfded21eb6cdb66debbf1e
2tankov2 <tan.kov@hotmail.com>
2018-06-04 13:22:02 +0300

    add exp, cond and procedures 1.1.1-1.1.6

---
+++ exercise-1_3.rkt
@@ -0,0 +1,7 @@
+(define (square x) (* x x))
+(define (sum-of-squeres x y)
+  (+ (square x) (square y)))
+(define  (sum-of-squeres-max a b c)
+  (cond ((and (>= a c) (>= b c)) (sum-of-squeres a b))
+        ((and (>= a b) (>= c b)) (sum-of-squeres a c))
+        (else (sum-of-squeres b c))))
